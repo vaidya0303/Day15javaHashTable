319c90c31f4d19364e861db9ee5f394fe209e1ca
vaidya0303 <vaidya0304@gmail.com>
2022-10-19 17:52:12 +0530

    remove avoidable word from phrase

--- src/com/Hashtable/MyHashTable.java
+++ src/com/Hashtable/MyHashTable.java
@@ -1,32 +1,49 @@
 package com.Hashtable;
 
+// import the arraylist
 import java.util.ArrayList;
 
 /**
- * Purpose - To create methods for Hash table features.
+ * create a class name as MyHashTable
+ * @param <K>key
+ * @param <V>value
  */
- class MyHashMap <K, V> {
+public class MyHashTable <K, V> {
+
     MyMapNode<K, V> head;
     MyMapNode<K, V> tail;
     private final int numOfBuckets;
     ArrayList<MyMapNode<K, V>> myBucketArray;
 
-    public MyHashMap() {
-        this.numOfBuckets = 10;
+    /**
+     * create a default constructor
+     *To initialize the  numOfBuckets
+     */
+
+    public MyHashTable() {
+        this.numOfBuckets = 20;  //20 is assigned a little more than the numbers of total words
         this.myBucketArray = new ArrayList<>(numOfBuckets);
-        // Create empty LinkedLists
+
+        /*
+         *  Create empty LinkedLists
+         * using for loop
+         */
         for (int i = 0; i < numOfBuckets; i++)
             this.myBucketArray.add(null);
     }
 
     /**
      * Purpose : method to get value from LinkedList using index number
-     *
      * @param key : key is returned
+     *
      */
 
     public V get(K key) {
         int index = this.getBucketIndex(key);
+
+        /*
+         * if condition is true then return is null
+         */
         if (this.myBucketArray.get(index) == null)
             return null;
         MyMapNode<K, V> myNode = search(key);
@@ -35,12 +52,16 @@ import java.util.ArrayList;
 
     /**
      * Purpose : Method to search the word in LinkedList
-     *
      * @param key : key to search
      */
 
     public MyMapNode<K, V> search(K key) {
         MyMapNode<K, V> currentNode = head;
+        /*
+         * using while loop
+         * current node is not empty.
+         * then again condition checked and return currentnode
+         */
         while (currentNode != null) {
             if (currentNode.getKey().equals(key)) {
                 return currentNode;
@@ -53,13 +74,14 @@ import java.util.ArrayList;
 
     /**
      * Purpose : Method to add key and value to hash table
-     *
-     * @param key    : word to be added
+     * @param key : word to be added
      * @param value: frequency of word
+     * The following method adds a key-value pair to the chain hash
      */
     public void add(K key, V value) {
         int index = this.getBucketIndex(key);
         MyMapNode<K, V> myNode = this.myBucketArray.get(index);
+
         if (myNode == null) {
             myNode = new MyMapNode<>(key, value);
             this.myBucketArray.set(index, myNode);
@@ -72,20 +94,18 @@ import java.util.ArrayList;
             myNode.setValue(value);
     }
 
-    /**
-     * this implements hash function to find index for a key
+    /*
+     *  This implements hash function to find index for a key
      */
     public int getBucketIndex(K key) {
         int hashCode = Math.abs(key.hashCode());
         int index = hashCode % numOfBuckets;
-        ////System.out.println("Key: "+word+" hashcode: "+hashCode+" index: "+index);
         return index;
 
     }
 
     /**
      * Purpose : Method to append value to Linked List
-     *
      * @param myNode : value to append
      */
 
@@ -98,6 +118,26 @@ import java.util.ArrayList;
             this.tail.setNext(myNode);
             this.tail = myNode;
         }
+    }
+
+    /**
+     * Purpose : Method to remove a word
+     * @param key : word to be removed
+     */
+    public void remove(K key) {
+        MyMapNode<K, V> currentNode = head;
+        MyMapNode<K, V> previousNode = null;
+        while (currentNode != null && currentNode.getKey().equals(key)) {
+            head = currentNode.getNext();
+        }
+        while (currentNode != null && !(currentNode.getKey().equals(key))) {
+            previousNode = currentNode;
+            currentNode = currentNode.getNext();
+        }
+        if (currentNode != null)
+            previousNode.next = currentNode.next;
+        if (currentNode == null)
+            System.out.println("Word not found");
     }
 
     @Override
